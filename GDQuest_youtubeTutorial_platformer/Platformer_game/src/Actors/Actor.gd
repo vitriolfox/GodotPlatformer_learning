786a8a67f2909e7fea482416e91f6b415d872a97
vitriolfox <vitriol.fox@gmail.com>
2019-11-22 16:02:21 +0100

    completed the first tile of the tutorial

--- GDQuest_youtubeTutorial_platformer/Platformer_game/src/Actors/Actor.gd
+++ GDQuest_youtubeTutorial_platformer/Platformer_game/src/Actors/Actor.gd
@@ -4,9 +4,8 @@ class_name Actor
 const FLOOR_NORMAL: = Vector2.UP
 export var speed: = Vector2(300, 1000)
 export var gravity: = 3000.0
-var velocity: = Vector2.ZERO
+var _velocity: = Vector2.ZERO
 
-func _physics_process(delta: float) -> void:
-	velocity.y += gravity * delta
 
-	
+
+
--- GDQuest_youtubeTutorial_platformer/Platformer_game/src/Actors/Player.gd
+++ GDQuest_youtubeTutorial_platformer/Platformer_game/src/Actors/Player.gd
@@ -1,10 +1,20 @@
 extends Actor
 
+
+export var stomp_impulse: = 1000.0
+
+func _on_EnemyDetector_area_entered(area: Area2D) -> void:
+	_velocity = calculate_stomp_velocity(_velocity, stomp_impulse)
+	
+func _on_EnemyDetector_body_entered(body: PhysicsBody2D) -> void:
+	queue_free()
+
+
 func _physics_process(delta: float) -> void:
-	var is_jump_interupted: = Input.is_action_just_released("move_jump") and velocity.y < 0.0
+	var is_jump_interupted: = Input.is_action_just_released("move_jump") and _velocity.y < 0.0
 	var direction: = get_direction()
-	velocity = calculate_move_velocity(velocity, direction, speed, is_jump_interupted)
-	velocity = move_and_slide(velocity, FLOOR_NORMAL)
+	_velocity = calculate_move_velocity(_velocity, direction, speed, is_jump_interupted)
+	_velocity = move_and_slide(_velocity, FLOOR_NORMAL)
 	
 func get_direction() -> Vector2:
 	return Vector2(
@@ -25,5 +35,13 @@ func calculate_move_velocity(
 		new_velocity.y = speed.y * direction.y
 	if is_jump_interupted: new_velocity.y = 0.0
 	return new_velocity
-	
-	
+
+
+func calculate_stomp_velocity(linear_velocity: Vector2, impulse: float) -> Vector2 :
+	var out: = linear_velocity
+	out.y = -impulse
+	return out
+
+
+
+
